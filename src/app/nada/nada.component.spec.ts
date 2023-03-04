9d594915e41e263a5cad2f4176a09eb37a4a1c6b
FelipeAlbertoAvilaCruz <felipealbertoavilacruz@gmail.com>
2023-03-04 01:15:07 -0600

    added_nadaModule

---
+++ src/app/nada/nada.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NadaComponent } from './nada.component';
+
+describe('NadaComponent', () => {
+  let component: NadaComponent;
+  let fixture: ComponentFixture<NadaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NadaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NadaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
